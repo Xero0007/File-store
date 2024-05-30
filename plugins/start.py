99680fbca94cb67b723100aef6504c7fb15ffcde
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-30 13:44:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,7 +3,6 @@
 import os
 import asyncio
 import logging
-import requests
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
