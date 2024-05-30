6734a0f7758b8b3b471756d4e826a76244de921f
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-30 13:02:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #IMDb 
-IMDB_API_KEY = ''
+IMDB_API_KEY = 'k_6p68dybu'
 
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7060558357:AAE2qAHq_KfDygVSvaAgxOAn7bGOAZcxXF0")
